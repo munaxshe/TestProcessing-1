7f318be80b8bbb227a3ad0b96ea189210aa861d3
Christos Frantzidis <cfrantzidis@lincoln.ac.uk>
2025-06-19 16:47:33 +0100

    Add project files.

---
+++ TestProcessing-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestProcessing_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gimmefile");
+            var z = Console.ReadLine();
+            var q = File.ReadAllLines(z);
+            Dictionary<string,int> a = new Dictionary<string,int>();
+            for (int i=0; i<q.Length; i++)
+            {
+                var s = Regex.Replace(q[i],@"[^\w\s]", "").ToLower();
+                var t = s.Split(' ');
+                for (int j=0;j<t.Length;j++) 
+                {
+                    var w = t[j];
+                    if (w != "")
+                    {
+                        if (a.ContainsKey(w))
+                            a[w] = a[w] + 1;    
+                        else
+                            a.Add(w, 1);
+                    }
+                }
+            }
+            Console.WriteLine("Done");
+            foreach (var e in a)
+            {
+                Console.WriteLine(e.Key + ":" + e.Value);
+            }
+            Console.WriteLine("uniquez = " + a.Count);
+            Console.ReadKey();
+        }
+    }
+}
